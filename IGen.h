02c5b16a33cc85d9c20be24328f17e392e254a46
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-04-16 22:01:30 +0300

    IGen.h added

---
+++ IGen.h
@@ -0,0 +1,46 @@
+#ifndef _IGen
+#define _IGen
+
+#include <random>
+#include <chrono>
+#include <type_traits>
+
+namespace IGen {
+    std::mt19937 mtrand (std::chrono::steady_clock::now ().time_since_epoch ().count ());
+
+    template <
+        typename T,
+        typename = typename std::enable_if<std::is_integral<T>::value, T>
+    >
+    T nextInt (T l, T r) {
+        std::uniform_int_distribution<T> uid (l, r);
+        return uid (mtrand);
+    }
+
+    template <
+        typename T,
+        typename = typename std::enable_if<std::is_integral<T>::value, T>
+    >
+    T nextInt (T r) {
+        return next (1, r);
+    }
+
+    template <
+        typename T,
+        typename = typename std::enable_if<std::is_floating_point<T>::value, T>
+    >
+    T nextFloat (T l, T r) {
+        std::uniform_real_distribution<T> urd (l, r);
+        return urd (mtrand);
+    }
+
+    template <
+        typename T,
+        typename = typename std::enable_if<std::is_floating_point<T>::value, T>  
+    >
+    T nextFloat (T r) {
+        return next (0., r);
+    }
+};
+
+#endif
--- gen.cpp
+++ gen.cpp
@@ -1,13 +1,12 @@
+#include "IGen.h"
 #include <bits/stdc++.h>
+
 using namespace std;
-mt19937 m((uint_fast32_t)clock()); 
-int randint (int a, int b) {
-    int x = 1;
-    for (int i = 0; i < 10000; i++)
-        x %= 10;
-    uniform_int_distribution<int> uid(a, b);
-    return uid(m);
-}
-int main () {
+
+signed main () {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cout.precision(14);
+
     
 }
