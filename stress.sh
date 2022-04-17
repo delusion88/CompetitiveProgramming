f222cce2bd6ca32b5da0e58648b037407ccb1cfb
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-04-17 20:40:09 +0300

    stress.sh and .vscode fixed

--- stress.sh
+++ stress.sh
@@ -11,10 +11,10 @@ NC='\033[0m'
 On_IBlue='\033[0;104m' 
 while [ $i -le $max_tests ]
 do
-    ./stress/generator > ./stress/input1.txt
-    ./stress/orig < ./stress/input1.txt > ./stress/original_output.txt
-    ./stress/brute < ./stress/input1.txt > ./stress/brute_output.txt
-    if diff --tabsize=1 -F --label --side-by-side --ignore-space-change ./stress/original_output.txt ./stress/brute_output.txt > ./stress/dont_show_on_terminal.txt; then
+    ./stress/generator > ./stress/input1
+    ./stress/orig < ./stress/input1 > ./stress/original_output
+    ./stress/brute < ./stress/input1 > ./stress/brute_output
+    if diff --tabsize=1 -F --label --side-by-side --ignore-space-change ./stress/original_output ./stress/brute_output > ./stress/dont_show_on_terminal; then
         echo -e "Test $i: ${GREEN}Accepted${NC}"
     else
         echo -e "Test $i: ${RED}Wrong Answer${NC}"
@@ -26,15 +26,20 @@ done
 if [ $diff_found -eq 1 ]
 then
     echo -e "${On_IBlue}Input:${NC}"
-    cat ./stress/input1.txt
+    cat ./stress/input1
     echo ""
     echo -e "${On_IBlue}Output:${NC}"
-    cat ./stress/original_output.txt
+    cat ./stress/original_output
     echo ""
     echo ""
     echo -e "${On_IBlue}Expected:${NC}"
-    cat ./stress/brute_output.txt
+    cat ./stress/brute_output
+    echo ""
     echo ""
 else
     echo -e "${GREEN}Ok ${max_tests} of ${max_tests} test cases.${NC}"
 fi
+rm -rf ./stress/original_output
+rm -rf ./stress/brute_output
+rm -rf ./stress/input1
+rm -rf ./stress/dont_show_on_terminal
