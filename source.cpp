fd99b1ed587387a37fe9e9bdba3eadb58332a7fd
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-01 12:49:14 +0300

    Oh no...

--- source.cpp
+++ source.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
 
+using ordered_set = 
+      __gnu_pbds::tree <
+          long long,
+          __gnu_pbds::null_type,
+          less <
+          long long >,
+          __gnu_pbds::rb_tree_tag,
+          __gnu_pbds::tree_order_statistics_node_update >;
+
+std::mt19937_64 rnd64 (
+    std::chrono::high_resolution_clock::now ().time_since_epoch ().count () );
+
+const int Inf       = 0x4f4f4f4f;
 const long long inf = 1100000000000000000;
-const int Inf = 0x4f4f4f4f;
 
 void solve_case () {
     
