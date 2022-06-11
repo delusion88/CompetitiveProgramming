0b5aebe40e9e935df2b95d41b60339aafa4ec6c3
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-11 22:30:16 +0300

    Update template

--- source.cpp
+++ source.cpp
@@ -3,18 +3,6 @@
 
 using namespace std;
 
-using ordered_set = 
-      __gnu_pbds::tree <
-          long long,
-          __gnu_pbds::null_type,
-          less <
-          long long >,
-          __gnu_pbds::rb_tree_tag,
-          __gnu_pbds::tree_order_statistics_node_update >;
-
-std::mt19937_64 rnd64 (
-    std::chrono::high_resolution_clock::now ().time_since_epoch ().count () );
-
 const int Inf       = 0x4f4f4f4f;
 const long long inf = 1100000000000000000;
 
