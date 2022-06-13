dce60b9c3c07f179fd1e073659a201e5d30969b1
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-13 14:32:03 +0300

    Update template

--- template.cpp
+++ template.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 const int Inf       = 0x4f4f4f4f;
 const long long inf = 1100000000000000000;
-
 void solve () {
     
 }
- 
 signed main () {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
     long long Tc = 1;
     // cin >> Tc;
-    for (long long testCase = 1; testCase <= Tc; ++testCase, cout << '\n') {
+    while(Tc--)
         solve();
-    }
 }
