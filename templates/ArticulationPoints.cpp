ebec258b68962797c9314b15e593a59d2ef9359b
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-16 14:40:38 +0300

    Add more templates

---
+++ templates/ArticulationPoints.cpp
@@ -0,0 +1,25 @@
+const int N = 300000;
+vector<int> g[N];
+int d[N], up[N];
+set<int> ans;
+void dfs (int v, int p, int dep) {
+    d[v] = up[v] = dep;
+    int ch = 0;
+    for (auto e : g[v]) {
+        if (e == p) continue;
+        if (!d[e]) {
+            dfs(e, v, dep + 1);
+            up[v] = min(up[v], up[e]);
+            if (d[v] <= up[e] && p != -1) {
+                ans.insert(v);
+            }
+            ch++;
+        }
+        else {
+            up[v] = min(up[v], d[e]);
+        }
+    }
+    if (p == -1 && ch > 1) {
+        ans.insert(v);
+    }
+}
---
+++ templates/Bridges.cpp
@@ -0,0 +1,20 @@
+const int N = 300000;
+vector<pair<int, int>> g[N];
+int d[N], up[N];
+vector<int> ans;
+void dfs (int v, int p, int dep) {
+    d[v] = up[v] = dep;
+    for (auto e : g[v]) {
+        if (e.first == p) continue;
+        if (d[e.first] == 0) {
+            dfs(e.first, v, dep + 1);
+            up[v] = min(up[v], up[e.first]);
+            if (up[e.first] >= d[e.first]) {
+                ans.push_back(e.second);
+            }
+        }
+        else {
+            up[v] = min(up[v], d[e.first]);
+        }
+    }
+}
