7a5491bc3e04cc05e79ca6ea6cedad1b69f8a65b
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-19 09:48:08 +0300

    Add Dijkstra

---
+++ templates/Dijkstra.cpp
@@ -0,0 +1,18 @@
+vector<long long> dijkstra(int n, int s = 0) {
+    vector<long long> d(n, inf);
+    d[s] = 0;
+    priority_queue<pair<long long, long long>, pair<long long, long long>, pair<long long, long long>> q;
+    q.push(make_pair(0ll, s));
+    while (!q.empty()) {
+        auto [cur_d, v] = q.top();
+        q.pop();
+        if (cur_d > d[v])
+            continue;
+        for (auto [u, w] : g[v]) {
+            if (d[u] > d[v] + w) {
+                d[u] = d[v] + w;
+                q.push(make_pair(d[u], u));
+            }
+        }
+    }
+}
