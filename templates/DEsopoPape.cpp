1fad87a092e37ffd3f4eb21a7419df25c7f36bfc
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-06-22 09:42:15 +0300

    Add something

---
+++ templates/DEsopoPape.cpp
@@ -0,0 +1,24 @@
+void shortest_paths(int n, int s, vector<long long>& d) {
+    d.assign(n, inf);
+    d[s] = 0;
+    vector<long long> m(n, 2);
+    deque<long long> q;
+    q.push_back(s);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop_front();
+        m[u] = 0;
+        for (auto [to, w] : g[u]) {
+            if (d[to] > d[u] + w) {
+                d[to] = d[u] + w;
+                if (m[to] == 2) {
+                    m[to] = 1;
+                    q.push_back(to);
+                } else if (m[to] == 0) {
+                    m[to] = 1;
+                    q.push_front(to);
+                }
+            }
+        }
+    }
+}
