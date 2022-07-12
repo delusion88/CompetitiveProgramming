dbf6f589dac78fc2b55b73e0a026a6cd00051a51
Ilya Surinov <fxtaco.ilya@gmail.com>
2022-07-12 20:03:40 +0300

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mkdir stress
+rm -rf ./.stress
+mkdir .stress
 i=1
-g++ -std=c++17 gen.cpp -o ./stress/generator
-g++ -std=c++17 source.cpp -o ./stress/orig
-g++ -std=c++17 brute.cpp -o ./stress/brute
+g++ -std=c++17 gen.cpp -o ./.stress/generator
+g++ -std=c++17 source.cpp -o ./.stress/orig
+g++ -std=c++17 brute.cpp -o ./.stress/brute
 max_tests=$1
 diff_found=0
 RED='\033[1;31m'
@@ -12,10 +13,10 @@ NC='\033[0m'
 On_IBlue='\033[0;104m' 
 while [ $i -le $max_tests ]
 do
-    ./stress/generator > ./stress/input1
-    ./stress/orig < ./stress/input1 > ./stress/original_output
-    ./stress/brute < ./stress/input1 > ./stress/brute_output
-    if diff --tabsize=1 -F --label --side-by-side --ignore-space-change ./stress/original_output ./stress/brute_output > ./stress/dont_show_on_terminal; then
+    ./.stress/generator > ./.stress/input1
+    ./.stress/orig < ./.stress/input1 > ./.stress/original_output
+    ./.stress/brute < ./.stress/input1 > ./.stress/brute_output
+    if diff --tabsize=1 -F --label --side-by-side --ignore-space-change ./stress/original_output ./stress/brute_output > ./.stress/dont_show_on_terminal; then
         echo -e "Test $i: ${GREEN}Accepted${NC}"
     else
         echo -e "Test $i: ${RED}Wrong Answer${NC}"
@@ -27,14 +28,14 @@ done
 if [ $diff_found -eq 1 ]
 then
     echo -e "${On_IBlue}Input:${NC}"
-    cat ./stress/input1
+    cat ./.stress/input1
     echo ""
     echo -e "${On_IBlue}Output:${NC}"
-    cat ./stress/original_output
+    cat ./.stress/original_output
     echo ""
     echo ""
     echo -e "${On_IBlue}Expected:${NC}"
-    cat ./stress/brute_output
+    cat ./.stress/brute_output
     echo ""
     echo ""
 else
